3d23d8910fa2ee5789b9d1a84b8cefdcbbbffb6e
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-03-18 17:03:57 -0400

    change colors based on current time fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,5 @@
 var blocks = $('.time-block');
 var save = $('.saveBtn');
-// var hour9 = $('#hourNine');
-// var hour10 = $('#hourTen');
-// var hour11 = $('#hourEleven');
-// var hour12 = $('#hourTwelwe');
-// var hour1 = $('#hourOne');
-// var hour2 = $('#hourTwo');
-// var hour3 = $('#hourThree');
-// var hour4 = $('#hourFour');
-// var hour5 = $('#hourFive');
 
 
 var currentDay = moment().format('dddd, MMM Do, YYYY');
@@ -38,12 +29,12 @@ $(document).ready(function () {
 
   //timeblocks for current day
   function timeTracker() {
+    //current hour
     var currentTime = moment().hour();
     console.log(currentTime);
 
     blocks.each(function () {
-      var currentBlock = parseInt($(this).attr('id').split('hour')[1]);
-      //console.log($('this'));
+      var currentBlock = parseInt($(this).attr('id').split('-')[1]);
       console.log(currentBlock)
 
       //past, present, future time background change

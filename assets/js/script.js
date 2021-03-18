8f52ca62dd6af7cc88a35bf8032c7c3f21dcbfeb
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-03-18 16:49:10 -0400

    hours id changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 var blocks = $('.time-block');
 var save = $('.saveBtn');
-var hour9 = $('#hourNine');
-var hour10 = $('#hourTen');
-var hour11 = $('#hourEleven');
-var hour12 = $('#hourTwelwe');
-var hour1 = $('#hourOne');
-var hour2 = $('#hourTwo');
-var hour3 = $('#hourThree');
-var hour4 = $('#hourFour');
-var hour5 = $('#hourFive');
+// var hour9 = $('#hourNine');
+// var hour10 = $('#hourTen');
+// var hour11 = $('#hourEleven');
+// var hour12 = $('#hourTwelwe');
+// var hour1 = $('#hourOne');
+// var hour2 = $('#hourTwo');
+// var hour3 = $('#hourThree');
+// var hour4 = $('#hourFour');
+// var hour5 = $('#hourFive');
 
 
 var currentDay = moment().format('dddd, MMM Do, YYYY');
@@ -24,9 +24,17 @@ $(document).ready(function () {
     localStorage.setItem(tasks, hours);
   })
   //info from local storage
-  hour9.val(localStorage.getItem('hourNine'));
-  hour10.val(localStorage.getItem('hourTen'))
-  console.log(hour9)
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+  console.log($('hour9'))
 
   //timeblocks for current day
   function timeTracker() {
@@ -35,7 +43,7 @@ $(document).ready(function () {
 
     blocks.each(function () {
       var currentBlock = parseInt($(this).attr('id').split('hour')[1]);
-      console.log($('this'));
+      //console.log($('this'));
       console.log(currentBlock)
 
       //past, present, future time background change
